97d2c23907ab2ee341a36c1ec49f74fbdb40b551
Mendezki <menluca@gmail.com>
2019-04-16 02:52:55 -0400

    deleted extra files from CRA

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 import "./Header.css"
 
 const Header = () => (
-    <div className="banner text-center d-flex align-items-center justify-content-center">
-        <p className="m-0">Click on a Loteria image to earn points, but don't repeat the image!</p>
+    <div className="header text-center d-flex align-items-center justify-content-center">
+        <p className="inst">Click on a Loteria image to earn points, but don't repeat the image!</p>
     </div>
 );
 export default Header;
